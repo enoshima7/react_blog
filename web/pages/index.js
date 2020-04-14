c82a9fb0c3faffba72d006f4217b32d2eb39a8c3
enoshima7 <duanjipeng0328@gmail.com>
2020-04-14 14:51:16 +0800

    first commit

---
+++ web/pages/index.js
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+function index() {
+  return (
+    <>
+      <div>HOME</div>
+      <div>
+        <Button>点击</Button>
+      </div>
+    </>
+  )
+}
+export default index
